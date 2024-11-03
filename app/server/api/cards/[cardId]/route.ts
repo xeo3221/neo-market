44f0e63af76325cb9067b53065a0cf0996ba9ad2
xeo3221 <95309495+xeo3221@users.noreply.github.com>
2024-11-04 00:00:00 +0100

    card routing error fix

--- app/server/api/cards/[cardId]/route.ts
+++ app/server/api/cards/[cardId]/route.ts
@@ -3,18 +3,16 @@ import { cards } from "@/drizzle/schema";
 import { eq } from "drizzle-orm";
 import { NextRequest, NextResponse } from "next/server";
 
-type Props = {
-  params: {
-    cardId: string;
-  };
-};
+type Params = Promise<{ cardId: string }>;
 
-export async function GET(request: NextRequest, { params }: Props) {
+export async function GET(request: NextRequest, context: { params: Params }) {
   try {
+    const { cardId } = await context.params;
+
     const [card] = await db
       .select()
       .from(cards)
-      .where(eq(cards.id, parseInt(params.cardId)));
+      .where(eq(cards.id, parseInt(cardId)));
 
     if (!card) {
       return new NextResponse("Card not found", { status: 404 });
